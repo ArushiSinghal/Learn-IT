eeb48e1f0100cff469209be6b9f1febce0db0406
Arushi Singhal <arushisinghal@gmail.com>
2017-10-24 07:57:34 +0530

    code

---
+++ code.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+import numpy
+import csv
+from csv import reader
+from matplotlib import pyplot
+import sys
---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install
+sudo apt-get install vim
+sudo apt-get install python-pip python3-pip
+sudo pip3 install -U numpy
+sudo pip install -U numpy
+sudo pip3 install --upgrade sqlparse
+sudo pip install matplotlib
+sudo pip3 install matplotlib
+sudo apt-get install python-tk
+sudo pip install --upgrade sqlparse
+python code.py
